c028773b719b53bc859e281c1c0d7bc89843c17a
unknown <acekun141@gmail.com>
2019-02-20 23:14:03 +0700

    remove mysqlclient

--- app/settings.py
+++ app/settings.py
@@ -77,15 +77,15 @@ WSGI_APPLICATION = 'app.wsgi.application'
 
 DATABASES = {
     'default': {
-        # 'ENGINE': 'django.db.backends.mysql',
-        # 'NAME': 'test',
-        # 'USER': 'root',
-        # 'PASSWORD': '',
-        # 'HOST':'localhost',
-        # 'PORT': '3325',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'test',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST':'localhost',
+        'PORT': '3325',
 
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
